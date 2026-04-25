018a2341a7bf0e15315107c3a06409891eb6756c
Jenny Li <root.jli@gmail.com>
2026-04-25 21:30:18 +0800

    Add wallet metadata fields to auth me response

--- backend/apps/accounts/wallet_prototype_urls.py
+++ backend/apps/accounts/wallet_prototype_urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from apps.accounts.views import WalletPrototypePayOrderAPIView
+from apps.accounts.views import WalletPrototypePayOrderAPIView, WalletPrototypePayProductOrderAPIView
 
 urlpatterns = [
     path('pay-order/', WalletPrototypePayOrderAPIView.as_view(), name='wallet-prototype-pay-order'),
+    path('pay-product-order/', WalletPrototypePayProductOrderAPIView.as_view(), name='wallet-prototype-pay-product-order'),
 ]
