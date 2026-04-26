0e3a3a8be6a213926afe1ae857a2b4b655dd06a0
Jenny Li <root.jli@gmail.com>
2026-04-26 20:41:52 +0800

    Expand admin settlement flow coverage for completed orders

---
+++ backend/apps/accounts/constants.py
@@ -0,0 +1,7 @@
+BLOCKCHAIN_NAME = "LTT"
+TOKEN_NAME = "LTT Thai Baht Stablecoin"
+TOKEN_SYMBOL = "THB-LTT"
+TOKEN_PEG = "1 THB-LTT = 1 THB"
+
+# Backward-compatible on-chain/storage code currently used in persisted records.
+LEGACY_CHAIN_CURRENCY_CODE = "LBC"
